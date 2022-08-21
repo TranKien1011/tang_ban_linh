45158f32bacc78fb4f8bc955e0cbc0536754bb38
TranKien1011 <trungkien101199@gmail.com>
2022-08-21 19:55:51 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Chào bạn Linh cute ! 🖐🖐🖐',
+    introDesc: `Có chuyện này muốn nói với bạn lâu rồi. Bạn muốn nghe chứ`,
+    btnIntro: 'OK',
+    title: 'Mình chơi game với nhau cũng lâu rồi ấy 😙',
+    desc: 'Mình thấy cũng hợp nhau ý. Hay là chúng ta thử tìm hiểu nhau xem sao ^^  ',
+    btnYes: 'Ulatr, giờ mới nói, làm người ta chờ mãi 💖💖💖',
+    btnNo: '💔 Bạn mơ à ! 💔 ',
+    question:'KKK. Vậy bạn thấy mình thế nào ? 😊😊',
+    reasonPlaceholder: 'Trả lời',
+    btnReply: 'Gửi ! 💌💌',
+    reply: 'Đẹp người, đẹp nết, chơi game toàn gánh mình <3 😊😊',
+    mess: 'Haha, mình biết mà 🥰. Vậy thử tìm hiểu nhau nhé 😘😘',
+    messDesc: '💬💬 Nhắn tin cho mình nha. 💬💬',
+    btnAccept: 'Ok luôn nè 💖',
+    messLink: 'https://m.me/caungocdangthuong'
+}
